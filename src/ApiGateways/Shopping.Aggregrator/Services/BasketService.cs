077e83e8ba4b8cb32feb34ecabf49dc5309e3b4d
saliqyousuf <sym12384@gmail.com>
2022-09-01 22:55:31 +0530

    shopping aggregrator

---
+++ src/ApiGateways/Shopping.Aggregrator/Services/BasketService.cs
@@ -0,0 +1,26 @@
+﻿using Shopping.Aggregrator.Extensions;
+using Shopping.Aggregrator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregrator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregrator/Services/ICatalogService.cs
@@ -0,0 +1,15 @@
+﻿using Shopping.Aggregrator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregrator.Services
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        Task<CatalogModel> GetCatalog(string id);
+    }
+}
